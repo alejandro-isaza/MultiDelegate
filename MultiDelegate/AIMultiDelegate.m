f778b91935ff0577cf398c1a1c2c681bd2f2a2a7
njuxjy <jxiao@defymedia.com>
2016-03-23 19:36:50 +0800

    Iterate delegates using a copy
    This is to prevent possible crashes if the delegates array is modified
    during iteration.

--- MultiDelegate/AIMultiDelegate.m
+++ MultiDelegate/AIMultiDelegate.m
@@ -68,7 +68,7 @@
 - (BOOL)respondsToSelector:(SEL)selector {
     if ([super respondsToSelector:selector])
         return YES;
-    
+
     for (id delegate in _delegates) {
         if (delegate && [delegate respondsToSelector:selector])
             return YES;
@@ -87,7 +87,7 @@
         // return any method signature, it doesn't really matter
         return [self methodSignatureForSelector:@selector(description)];
     }
-    
+
     for (id delegate in _delegates) {
         if (!delegate)
             continue;
@@ -104,7 +104,8 @@
     SEL selector = [invocation selector];
     BOOL responded = NO;
     
-    for (id delegate in _delegates) {
+    NSArray *copiedDelegates = [_delegates copy];
+    for (id delegate in copiedDelegates) {
         if (delegate && [delegate respondsToSelector:selector]) {
             [invocation invokeWithTarget:delegate];
             responded = YES;
